e72de8dc9d35ece408b111059234d0a15096ca36
thullurivamsi <thullurivamsi51@gmail.com>
2025-08-04 17:37:59 +0530

    Create Customer.java

---
+++ src /java /com.orderprocessing /model /Customer.java
@@ -0,0 +1,27 @@
+package com.orderprocessing.model;
+
+public class Customer {
+    private String name;
+    private int id;
+
+    public Customer(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
